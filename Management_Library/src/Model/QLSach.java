99ce4f85e8841ecf5ddb1fc5a229e6ce0fd2d077
Le Thanh Tam <tam1442k3@gmail.com>
2023-03-12 20:38:03 +0700

    all

--- Management_Library/src/Model/QLSach.java
+++ Management_Library/src/Model/QLSach.java
@@ -1,13 +1,14 @@
 package Model;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class QLSach {
-	private Map<Sach,Integer> listBook ;
+	private Map<Sach, Integer> listBook;
 
 	public QLSach() {
-		this.listBook = new HashMap<Sach,Integer>();
+		this.listBook = new HashMap<Sach, Integer>();
 	}
 
 	public Map<Sach, Integer> getListBook() {
@@ -17,6 +18,20 @@ public class QLSach {
 	public void setListBook(Map<Sach, Integer> listBook) {
 		this.listBook = listBook;
 	}
-	
-	
+
+	public Sach timSach(String tenSach) {
+
+		return null;
+	}
+
+	public boolean themSach(String idSach, String tenSach, TacGia tacGia, String theLoai, NhaXuatBan nhaXuatBan,
+			Date namXuatBan) {
+
+		return false;
+	}
+
+	public boolean xoaSach(String tenSach) {
+		return false;
+
+	}
 }
--- Management_Library/src/Model/TacGia.java
+++ Management_Library/src/Model/TacGia.java
@@ -1,5 +1,51 @@
 package Model;
 
+import java.util.Date;
+
 public class TacGia {
+	private String idTacGia;
+	private String tenTacGia;
+	private Date ngaysinh;
+	private String queQuan;
+
+	public TacGia(String idTacGia, String tenTacGia, Date ngaysinh, String queQuan) {
+		super();
+		this.idTacGia = idTacGia;
+		this.tenTacGia = tenTacGia;
+		this.ngaysinh = ngaysinh;
+		this.queQuan = queQuan;
+	}
+
+	public String getIdTacGia() {
+		return idTacGia;
+	}
+
+	public void setIdTacGia(String idTacGia) {
+		this.idTacGia = idTacGia;
+	}
+
+	public String getTenTacGia() {
+		return tenTacGia;
+	}
+
+	public void setTenTacGia(String tenTacGia) {
+		this.tenTacGia = tenTacGia;
+	}
+
+	public Date getNgaysinh() {
+		return ngaysinh;
+	}
+
+	public void setNgaysinh(Date ngaysinh) {
+		this.ngaysinh = ngaysinh;
+	}
+
+	public String getQueQuan() {
+		return queQuan;
+	}
+
+	public void setQueQuan(String queQuan) {
+		this.queQuan = queQuan;
+	}
 
 }
