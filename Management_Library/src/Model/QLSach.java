59d601d8eb14219a74ca7fcd753129e0f2f7ddae
Le Thanh Tam <tam1442k3@gmail.com>
2023-03-12 23:34:01 +0700

    12/03

--- Management_Library/src/Model/QLSach.java
+++ Management_Library/src/Model/QLSach.java
@@ -5,32 +5,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class QLSach {
-	private Map<Sach, Integer> listBook;
+	private Map<Sach, Integer> dsSach;
 
 	public QLSach() {
-		this.listBook = new HashMap<Sach, Integer>();
+		this.dsSach = new HashMap<Sach, Integer>();
 	}
 
 	public Map<Sach, Integer> getListBook() {
-		return listBook;
+		return dsSach;
 	}
 
 	public void setListBook(Map<Sach, Integer> listBook) {
-		this.listBook = listBook;
+		this.dsSach = listBook;
 	}
 
 	public Sach timSach(String tenSach) {
-
-		return null;
+		Sach sach = null;
+		for (Sach s : dsSach.keySet()) {
+			if (s.getTenSach().equals(tenSach)) {
+				sach = s;
+			}
+		}
+		return sach;
 	}
 
-	public boolean themSach(String idSach, String tenSach, TacGia tacGia, String theLoai, NhaXuatBan nhaXuatBan,
+	public void themSach(String idSach, String tenSach, TacGia tacGia, String theLoai, NhaXuatBan nhaXuatBan,
 			Date namXuatBan) {
-
-		return false;
+		Sach s = new Sach(idSach, tenSach, tacGia, theLoai, nhaXuatBan, namXuatBan);
+		if (s != null) {
+			dsSach.put(s, 0);
+		}
 	}
 
 	public boolean xoaSach(String tenSach) {
+		Sach s = timSach(tenSach);
+		if (dsSach.containsKey(s)) {
+			dsSach.remove(s);
+			return true;
+		}
 		return false;
 
 	}
