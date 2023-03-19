e2465eaca2477ee5c2506315921117c0a7c2fac6
Le Thanh Tam <tam1442k3@gmail.com>
2023-03-19 21:56:40 +0700

    HELO

---
+++ Management_Library/src/View/viewMain.java
@@ -0,0 +1,12 @@
+package View;
+
+import javax.swing.JFrame;
+
+public class viewMain extends JFrame {
+	public viewMain() {
+		// TODO Auto-generated constructor stub
+		this.getPreferredSize();
+		this.pack();
+		this.setVisible(true);
+	}
+}
---
+++ Management_Library/src/View/view_QuanLyDocGia.java
@@ -0,0 +1,15 @@
+package View;
+
+import javax.swing.JFrame;
+
+public class view_QuanLyDocGia extends JFrame {
+	public view_QuanLyDocGia() {
+		// TODO Auto-generated constructor stub
+		this.init();
+	}
+
+	private void init() {
+		// TODO Auto-generated method stub
+		
+	}
+}
