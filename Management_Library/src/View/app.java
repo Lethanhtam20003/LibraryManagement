b22d88c052f1f6553f734418d87bf2277097e909
Le Thanh Tam <tam1442k3@gmail.com>
2023-05-11 18:50:11 +0700

    Update app.java

--- Management_Library/src/View/app.java
+++ Management_Library/src/View/app.java
@@ -22,23 +22,43 @@ public class app extends JFrame {
 		this.init();
 		
 		
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocation(0, 0);
-		this.pack();
+		/*  ***********pack() 
+		 *  Nó cố gắng tối thiểu hóa khoảng trống giữa các thành phần bên trong 
+		 *  và đảm bảo rằng tất cả các thành phần đều được hiển thị trên JFrame hoặc JDialog.
+		 *  
+		 *  ***cách hoạt động của pack() 
+		 *  b1: Tính toán lại kích thước và vị trí của JFrame hoặc JDialog dựa trên kích thước 
+		 *  và vị trí của các thành phần bên trong.
+		 *  b2:Đặt lại kích thước và vị trí của JFrame hoặc JDialog theo kết quả tính toán được.
+		 *  
+		 *  ***thường được gọi sau khi thêm tất cả các thành phần vào JFrame hoặc JDialog 
+		 *  và trước khi hiển thị nó lên màn hình. Khi bạn gọi phương thức pack(), 
+		 *  nó sẽ đảm bảo rằng JFrame hoặc JDialog của bạn được hiển thị đúng kích thước 
+		 *  và vị trí của nó.
+		 */
+		this.pack(); 
+		
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);// làm cho jframe full màn hình
+
 		this.setVisible(true);
 	}
 
 	private void init() {
 		// TODO Auto-generated method stub
-
+		// font 
 		Font font = new Font("Arial", Font.BOLD, 30);
 		this.setFont(font);
-		containpane = new JPanel();
-		containpane.setLayout(new BorderLayout());
+		
+		// menu
 		this.setJMenuBar(createMenu());
 
+		// contaipane 
+		containpane = new JPanel();
+		containpane.setLayout(new BorderLayout());
 		this.add(new view_QuanLyDocGia(), BorderLayout.CENTER);
+		
 
 	}
 
