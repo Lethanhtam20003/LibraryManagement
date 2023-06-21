832ef602eb809cbb7b06816f658ccc0f4bbf94bc
21130459 <127572964+21130459@users.noreply.github.com>
2023-06-21 11:55:21 +0700

    Update QLPhieuMuonTra.java

--- Management_Library/src/model/QLPhieuMuonTra.java
+++ Management_Library/src/model/QLPhieuMuonTra.java
@@ -4,14 +4,10 @@ import java.util.List;
 
 public class QLPhieuMuonTra implements ThuThu {
 	List<PhieuMuon>listPhieuMuon;
-	QLSach qlSach;
-	
 
-	public QLPhieuMuonTra(List<PhieuMuon> listPhieuMuon, QLSach qlSach) {
+	public QLPhieuMuon(List<PhieuMuon> listPhieuMuon) {
 		super();
-//		listPhieuMuon=new ArrayList<>();
 		this.listPhieuMuon = listPhieuMuon;
-		this.qlSach = qlSach;
 	}
 
 	public List<PhieuMuon> getListPhieuMuon() {
@@ -21,47 +17,20 @@ public class QLPhieuMuonTra implements ThuThu {
 	public void setListPhieuMuon(List<PhieuMuon> listPhieuMuon) {
 		this.listPhieuMuon = listPhieuMuon;
 	}
-
-	@Override
-	public List<Object> timKiemTheoID(String id) {
-		// TODO Auto-generated method stub
-		return null;
+	public void xoaPhieu(String id) {
+		PhieuMuon phieuMuon = timPhieu(id);
+		if(phieuMuon !=null) {
+			listPhieuMuon.remove(phieuMuon);
+		}	
 	}
-
-	@Override
-	public List<Object> timKiemTheoTen(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void them(Object object) {
-		
-		PhieuMuon obj = (PhieuMuon) object;
-		listPhieuMuon.add(obj);
-		for(Sach s: qlSach.listSach) {
-			
-			if(obj.sach.getiD().equals(s.getiD())) {
-				s.setSoLuongChoMuon(s.getSoLuongChoMuon()+1);
+	public void capNhatPhieu(String idPhieu, List<Sach> listSachMuonMoi, Date ngayMuonMoi, Date ngayDuKienTraMoi) {
+		PhieuMuon phieuMuon = timPhieu(idPhieu);
+		if(phieuMuon != null) {
+			if(phieuMuon.getTrangThai().equals("Da Tra")) {
+				phieuMuon.setListSachMuon(listSachMuonMoi);
+				phieuMuon.setNgayMuon(ngayMuonMoi);
+				phieuMuon.setNgayDuKienTra(ngayDuKienTraMoi);
 			}
 		}
-	}
-
-	@Override
-	public void xoa(String id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void capNhat(String id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getSoLuong() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 }
