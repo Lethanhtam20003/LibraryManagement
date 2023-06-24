ff7591b7ee8ab396a9f02076856ae3b5744422d8
HuuNhan <127572964+21130459@users.noreply.github.com>
2023-06-24 19:35:23 +0700

    Update QLPhieuMuonTra.java
     xóa+cập nhật phiếu

--- Management_Library/src/model/QLPhieuMuonTra.java
+++ Management_Library/src/model/QLPhieuMuonTra.java
@@ -1,20 +1,15 @@
 package model;
 
+import java.util.Date;
 import java.util.List;
-
 
 public class QLPhieuMuonTra implements QuanLyThuVien {
 
+List<PhieuMuon>listPhieuMuon;
 
-	List<PhieuMuon>listPhieuMuon;
-	QLSach qlSach;
-	
-
-	public QLPhieuMuonTra(List<PhieuMuon> listPhieuMuon, QLSach qlSach) {
+	public QLPhieuMuonTra(List<PhieuMuon> listPhieuMuon) {
 		super();
-//		listPhieuMuon=new ArrayList<>();
 		this.listPhieuMuon = listPhieuMuon;
-		this.qlSach = qlSach;
 	}
 
 	public List<PhieuMuon> getListPhieuMuon() {
@@ -24,7 +19,7 @@ public class QLPhieuMuonTra implements QuanLyThuVien {
 	public void setListPhieuMuon(List<PhieuMuon> listPhieuMuon) {
 		this.listPhieuMuon = listPhieuMuon;
 	}
-
+	
 	@Override
 	public List<Object> timKiemTheoID(String id) {
 		// TODO Auto-generated method stub
@@ -51,17 +46,24 @@ public class QLPhieuMuonTra implements QuanLyThuVien {
 	}
 
 	@Override
-	public void xoa(String id) {
-		// TODO Auto-generated method stub
-		
+	public void xoaPhieu(String id) {
+		PhieuMuon phieuMuon = timPhieu(id);
+		if(phieuMuon !=null) {
+			listPhieuMuon.remove(phieuMuon);
+		}	
 	}
 
 	@Override
-	public void capNhat(String id) {
-		// TODO Auto-generated method stub
-		
+	public void capNhatPhieu(String idPhieu, List<Sach> listSachMuonMoi, Date ngayMuonMoi, Date ngayDuKienTraMoi) {
+		PhieuMuon phieuMuon = timPhieu(idPhieu);
+		if(phieuMuon != null) {
+			if(phieuMuon.getTrangThai().equals("Da Tra Sach")) {
+				phieuMuon.setListSachMuon(listSachMuonMoi);
+				phieuMuon.setNgayMuon(ngayMuonMoi);
+				phieuMuon.setNgayDuKienTra(ngayDuKienTraMoi);
+			}
+		}
 	}
-
 	@Override
 	public int getTongSoLuong() {
 		// TODO Auto-generated method stub
