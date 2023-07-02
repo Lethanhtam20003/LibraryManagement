32e420fa46b35c620d3d06d3f03c0d65cebadae0
Lê Thanh Tâm <tam1442k3@gmail.com>
2023-07-02 19:14:30 +0700

    debug

--- Management_Library/src/dao/DataPhieuMuon.java
+++ Management_Library/src/dao/DataPhieuMuon.java
@@ -17,9 +17,9 @@ public class DataPhieuMuon {
 		listSach3.add("Lập trình nâng cao");
 		listSach3.add("Cấu trúc dữ liệu");
 
-		PhieuMuon p1 = new PhieuMuon("1", "KH101", listSach1, "20/6/2023", "22/6/2023", 1000);
-		PhieuMuon p2 = new PhieuMuon("2", "KH102", listSach2, "20/6/2023", "23/6/2023", 1000);
-		PhieuMuon p3 = new PhieuMuon("3", "KH101", listSach3, "21/6/2023", "24/6/2023", 2000);
+		PhieuMuon p1 = new PhieuMuon("1", "KH101", listSach1, "20/6/2023", "22/6/2023", 1000, "Chưa trả sách");
+		PhieuMuon p2 = new PhieuMuon("2", "KH102", listSach2, "20/6/2023", "23/6/2023", 1000, "Đã trả sách");
+		PhieuMuon p3 = new PhieuMuon("3", "KH101", listSach3, "21/6/2023", "24/6/2023", 2000, "Chưa trả sách");
 
 		listPhieu.add(p1);
 		listPhieu.add(p2);
